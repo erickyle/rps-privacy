b109e0b8687f517b0557f1b3c1575a55380997c6
Eric Kyle <eric@redpixel.com>
2018-03-09 12:21:18 -0600

    * Update require directive.

--- plugin.php
+++ plugin.php
@@ -96,7 +96,7 @@ spl_autoload_register( function ( $class ) {
 	$path = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . implode( DIRECTORY_SEPARATOR, $class_parts ) . '.php';
 		
 	if ( file_exists( $path ) ) {
-		require( $path );
+		require $path;
 	}
 
 });
